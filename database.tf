809d93a0bb1ca1f7529b48f30cff3fbf5abf8e96
monmichael32 <monmichael32@gmail.com>
2020-09-04 08:52:01 -0600

    infra 2 update

--- database.tf
+++ database.tf
@@ -12,63 +12,8 @@ resource "aws_instance" "mongodb_one" {
         volume_type = "gp2"
         volume_size = "100"
     }
-    
-    vpc_security_group_ids = [aws_security_group.mongodb.id]
-    associate_public_ip_address = true
-    key_name = "deployer-key"
-    subnet_id=aws_subnet.subnet_public_a.id
 }
 
-resource "aws_security_group" "mongodb" {
-  name        = "mongodb"
-  description = "Security group for mongodb"
- 
-  vpc_id=aws_vpc.vpc.id
-
-  tags = {
-    Name = "mongodb-prod"
-  }
-}
-
-resource "aws_security_group_rule" "mongodb_allow_all" {
-  type            = "egress"
-  from_port       = 0
-  to_port         = 0
-  protocol        = "-1"
-  cidr_blocks     = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.mongodb.id
-}
-
-resource "aws_security_group_rule" "mongodb_ssh" {
-  type            = "ingress"
-  from_port       = 22
-  to_port         = 22
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.mongodb.id
-}
-
-resource "aws_security_group_rule" "mongodb_mongodb" {
-  type            = "ingress"
-  from_port       = 27017
-  to_port         = 27017
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.mongodb.id
-}
-
-resource "aws_security_group_rule" "mongodb_mongodb_replication" {
-  type            = "ingress"
-  from_port       = 27019
-  to_port         = 27019
-  protocol        = "tcp"
-  cidr_blocks     = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.mongodb.id
-}
 resource "aws_s3_bucket" "cts-web-resources" {
     bucket = "cts-web-resources"
     acl    = "public-read"
@@ -86,19 +31,3 @@ resource "aws_lb_target_group_attachment" "mongodb_one" {
   port = 27017
 }
 
-#data "terraform_remote_state" "network" {
-  #backend = "s3"
-  #config = {
-    #bucket = "cts-web-resources"
-    #key    = "anaible-apache/files/eureka.tar"
-    #region = "us-east-1"
-  #}
-#}
-
-#terraform {
-  #backend "consul" {
-    #bucket = "cts-web-resources"
-    #key    = "eureka.tar"
-    #region = "us-east-1"
-  #}
-#}
